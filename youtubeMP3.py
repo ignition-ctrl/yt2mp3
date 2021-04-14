e33cece20057f526506df2b933435a1b5fbc99b7
Zack <akazackt@gmail.com>
2021-04-14 23:28:09 +0000

    updated youtubemp3.py

--- youtubeMP3.py
+++ youtubeMP3.py
@@ -1,16 +1,22 @@
 import youtube_dl
 import sys
 
+arguments = sys.argv[1:]
+filename = arguments[1]
+arguments.pop()
+ytname = arguments[0:]
+
 params = {
-        'format': 'bestaudio/best',
+        'format': 'bestaudio/best',        
         'postprocessors':[{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
-        }]
-
-}
+        }],
+        'outtmpl': "/home/server-akaza/pythondir/yt2mp3/"+str(filename)+".mp3"
+        }
 
 youtube = youtube_dl.YoutubeDL(params)
-youtube.download(sys.argv[1:])
+
+youtube.download(ytname)
 
