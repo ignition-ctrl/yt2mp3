4eb71d1ec2ba72599ae82d9c9813456b3f7cd7da
ignition-ctrl <akazackt@gmail.com>
2021-05-19 15:16:40 -0400

    cleaned up code, added ternary operator in params, and added inputs

--- youtubeMP3.py
+++ youtubeMP3.py
@@ -5,41 +5,29 @@ import getpass
 sys.path.insert(1, '~/pythondir/yt2mp3/thumbnails')
 from downloadimg import download_thumbnail
 
-if len(sys.argv) < 3:
-    print("File will be downloaded in ~/pythondir/yt2mp3/downloaded")
-    if len(sys.argv) == 2:
-        print("Please provide a filename without .mp3\nABORTING")
-        exit(1)
-    elif len(sys.argv) == 1:
-        print("Please provide a Youtube link and a filename without .mp3\nABORTING")
-        exit(1)
-    else:
-        print("Please provide only a YT link and a filename (without .mp3) separated by a single space.")
+if len(sys.argv) == 2:
+    print("Please provide a filename without .mp3\nABORTING")
+    exit(1)
+elif len(sys.argv) == 1:
+    print("Please provide a Youtube link and a filename without .mp3\nABORTING")
+    exit(1)
+else:
+    print("Please provide only a YT link and a filename (without .mp3) separated by a single space.")
 
-def download_music(ytname, filename, location):
+def download_music(ytname):
     name = getpass.getuser()
-    if str(location) == "here":
-        filedir = os.getcwd()
-        params = {
+    filename = input("Please enter your desired filename for the .mp3 file\n")
+    heredeterminer = input("Do you want to download the file in the current directory?")
+    filedir = os.getcwd()
+    params = {
             'format': 'bestaudio/best',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
+                'preferredcodec': 'mp3', 
                 'preferredquality': '192',
-            }],
+                }],
             'source_address':'0.0.0.0',
-            'outtmpl': str(filedir) + "/" + str(filename) + "."
-}
-    else:
-        params = {
-                'format': 'bestaudio/best',
-                'postprocessors': [{
-                    'key': 'FFmpegExtractAudio',
-                    'preferredcodec': 'mp3', 
-                    'preferredquality': '192',
-                    }],
-                'source_address':'0.0.0.0',
-                'outtmpl': "/home/" + name + "/pythondir/yt2mp3/downloaded/" + str(filename) + "."
+            'outtmpl':  str(filedir) + "/" + str(filename) + "." if heredeterminer == "yes" or "Yes" or "y" else "/home/" + name + "/pythondir/yt2mp3/downloaded/" + str(filename) + "."
 }
     youtube = youtube_dl.YoutubeDL(params)
     youtube.download([ytname])
@@ -54,8 +42,10 @@ def download_music(ytname, filename, location):
         exit(0)
     else:
         exit(1)
+
+
 if __name__ == '__main__':
     if len(sys.argv) == 3:
-        download_music(sys.argv[1], sys.argv[2], '')
+        download_music(sys.argv[1])
     else:
-        download_music(sys.argv[1], sys.argv[2], sys.argv[3])
+        download_music(sys.argv[1])
