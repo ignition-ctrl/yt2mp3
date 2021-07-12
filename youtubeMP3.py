4db7d58f86bb50abb12a1915d5dcb2a5c146e793
ignition-ctrl <akazackt@gmail.com>
2021-07-12 12:58:47 -0400

    created different files with commentary

--- youtubeMP3.py
+++ youtubeMP3.py
@@ -6,54 +6,28 @@ import getpass
 sys.path.insert(1, '~/pythondir/yt2mp3/thumbnails')
 from downloadimg import download_thumbnail
 import eyed3
-"""
-I am such a genius. I love this. Future Zack, (who hopefully got a job as a Linux SysAdmin, and is living the dream), this is how the
-code works. I started with the basic formula, creating a function with a params dictionary to send to youtube-dl to download an mp3.
-However, I started customizing it. I didn't want to have long filenames with the video id. (Though I later found out there was an outtmpl
-option to remove the video id,) I made some changes to the argv passing so I could add a filename argument after the link. I was so proud.
-I think I even edited the source code of youtube-dl to remove an annoying outtmpl error. Eventually, I added little things, like adding
-a bash function to call the script anywhere, the option to download in the current directory, and even made a secondary program to 
-download thumbnails. Also finding the user's name to accurately put songs in the downloaded folder (though I could have just used ~; 
-story of my life). All of this, to culminate in the complete shipping of Zack's youtubeMP3 1.0. Here's how it works.
-"""
+
 
 def download_music(ytname=None, filename=None, heredeterminer=None):
-    #So, right off the bat, this function takes three arguments, but optionally! I set each argument as None, and added
-    #behavior to interactively supply any missing parts. Genius, I know
-    if ytname == None: #so, here it is. If ytname is None, interactively have the user put a link in, and if it's not valid exit out.
+    
+    if ytname == None: 
         linkfinder = input("Please enter your link\n")
         if re.match("http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?", linkfinder):
-            #IGNOREdownload_music(str(linkfinder), filename, heredeterminer) #This sends the user through the function again, with linkfinder taking ytname
-            """
-            actually, these comments are wrong. After I finished writing them, I realized instead of creating more iterations, I can just
-            reassign the variable to the input. That way, it's faster and only runs once. I can be such an idiot. Also, fixed some sys.argv
-            errors. It works perfectly now. 1.0 is ready.
-            """
             ytname = str(linkfinder)
-            #IGNOREsince linkfinder is not None, it won't trigger this code again.
-            #IGNOREprint("exit")
-            #IGNOREexit(0) #However, once download_music finished, it will continue at the first iteration. The exit guarantees there's no loop
         else:
             print("No link found; please try again")
             exit(1)
-    name = getpass.getuser() #just a way to get the name so that it can be put in the full outtmpl path. I guess I can just use ~, though
+    name = getpass.getuser() 
     if filename == None:
         namefinder = input("Please enter your desired filename for the .mp3 file\n")
-        #IGNOREdownload_music(ytname, str(namefinder), heredeterminer)
         filename = str(namefinder)
-        #IGNOREprint("exit")
-        #IGNOREexit(0) # same sort of None verifying. It's really genius, whoever put this in python
     if heredeterminer == None:
         herefinder = input("Do you want to download the file in the current directory?\n")
-        #IGNOREdownload_music(ytname, filename, str(herefinder)) #Now, at this point, this should be the final iteration of the function, since all arguments are supplied
         heredeterminer = str(herefinder)
-        #IGNOREprint("exit")
-        #IGNOREexit(0) #should be the final exit out of the iterations
-    filedir = os.getcwd() #just incase they want to download in the current directory
+    filedir = os.getcwd() 
     defaultname = False
     if filename == "":
         defaultname = True
-    #added the above in order to use ternary operators to include optional default filenames
     cwdpath = (str(filedir) + "/" + "%(title)s.%(ext)s") if defaultname == True else (str(filedir) + "/" + str(filename) + ".%(ext)s")
     defaultpath = ("/home/" + name + "/pythondir/yt2mp3/downloaded/" + "%(title)s.%(ext)s") if defaultname == True else ("/home/" + name + "/pythondir/yt2mp3/downloaded/" + str(filename) + "%.(ext)s")
     params = {
@@ -63,12 +37,10 @@ def download_music(ytname=None, filename=None, heredeterminer=None):
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3', 
                 'preferredquality': '192',},
-                {'key': 'EmbedThumbnail'},],# this makes thumbnaildownload obsolete, but I'll keep it in case.
+                {'key': 'EmbedThumbnail'},],
             'cookiefile' : '~/pythondir/yt2mp3/youtube.com_cookies.txt',
             'source_address':'0.0.0.0',
             'outtmpl':  (cwdpath) if heredeterminer == "yes" or "Yes" or "y" else (defaultpath)
-            #above is a really long line, with ternary operators. Useful for changing outtmpl's definition
-            #updated, put the paths in seperate variables with ternary operators of their own, for optional default names.
 }
     youtube = youtube_dl.YoutubeDL(params) #the bulk of the work is made by youtube-dl.
     ytname = str(ytname)
@@ -90,9 +62,8 @@ def download_music(ytname=None, filename=None, heredeterminer=None):
     trackfinder = input("Track name?\n")
     audiofile.tag.title = str(trackfinder)
     audiofile.tag.save()
-    #Some simple querying in order to put the right tags. 
     print("The thumbnail has been downloaded and embedded within the file. However, you can still download it seperately.")
-    determiner = input("Do you want to download the thumbnail?\n") #a little interactive determiner to download the thumbnail
+    determiner = input("Do you want to download the thumbnail?\n") 
     if (determiner == "yes"):
         seconddeter = input("Do you want to download it in the current directory?\n")
         if seconddeter == "yes":
@@ -117,8 +88,3 @@ if __name__ == '__main__':
                 download_music(sys.argv[1])
     else:
         download_music()
-        #here, if I'm running the script directly, it tries running it with command line arguments
-        #Of course, if they are no arguments it throws an IndexError. So it moves on 
-        #here, now it just calls it without any sys.argv. everything is set to None, and the iterations go to work.
-
-#That's it! It should all be good now, so enjoy working on whatever projects you're on now, Zack!
---
+++ ytmp3-commentary.py
@@ -0,0 +1,124 @@
+import re
+import youtube_dl
+import sys
+import os
+import getpass
+sys.path.insert(1, '~/pythondir/yt2mp3/thumbnails')
+from downloadimg import download_thumbnail
+import eyed3
+"""
+I am such a genius. I love this. Future Zack, (who hopefully got a job as a Linux SysAdmin, and is living the dream), this is how the
+code works. I started with the basic formula, creating a function with a params dictionary to send to youtube-dl to download an mp3.
+However, I started customizing it. I didn't want to have long filenames with the video id. (Though I later found out there was an outtmpl
+option to remove the video id,) I made some changes to the argv passing so I could add a filename argument after the link. I was so proud.
+I think I even edited the source code of youtube-dl to remove an annoying outtmpl error. Eventually, I added little things, like adding
+a bash function to call the script anywhere, the option to download in the current directory, and even made a secondary program to 
+download thumbnails. Also finding the user's name to accurately put songs in the downloaded folder (though I could have just used ~; 
+story of my life). All of this, to culminate in the complete shipping of Zack's youtubeMP3 1.0. Here's how it works.
+"""
+
+def download_music(ytname=None, filename=None, heredeterminer=None):
+    #So, right off the bat, this function takes three arguments, but optionally! I set each argument as None, and added
+    #behavior to interactively supply any missing parts. Genius, I know
+    if ytname == None: #so, here it is. If ytname is None, interactively have the user put a link in, and if it's not valid exit out.
+        linkfinder = input("Please enter your link\n")
+        if re.match("http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?", linkfinder):
+            #IGNOREdownload_music(str(linkfinder), filename, heredeterminer) #This sends the user through the function again, with linkfinder taking ytname
+            """
+            actually, these comments are wrong. After I finished writing them, I realized instead of creating more iterations, I can just
+            reassign the variable to the input. That way, it's faster and only runs once. I can be such an idiot. Also, fixed some sys.argv
+            errors. It works perfectly now. 1.0 is ready.
+            """
+            ytname = str(linkfinder)
+            #IGNOREsince linkfinder is not None, it won't trigger this code again.
+            #IGNOREprint("exit")
+            #IGNOREexit(0) #However, once download_music finished, it will continue at the first iteration. The exit guarantees there's no loop
+        else:
+            print("No link found; please try again")
+            exit(1)
+    name = getpass.getuser() #just a way to get the name so that it can be put in the full outtmpl path. I guess I can just use ~, though
+    if filename == None:
+        namefinder = input("Please enter your desired filename for the .mp3 file\n")
+        #IGNOREdownload_music(ytname, str(namefinder), heredeterminer)
+        filename = str(namefinder)
+        #IGNOREprint("exit")
+        #IGNOREexit(0) # same sort of None verifying. It's really genius, whoever put this in python
+    if heredeterminer == None:
+        herefinder = input("Do you want to download the file in the current directory?\n")
+        #IGNOREdownload_music(ytname, filename, str(herefinder)) #Now, at this point, this should be the final iteration of the function, since all arguments are supplied
+        heredeterminer = str(herefinder)
+        #IGNOREprint("exit")
+        #IGNOREexit(0) #should be the final exit out of the iterations
+    filedir = os.getcwd() #just incase they want to download in the current directory
+    defaultname = False
+    if filename == "":
+        defaultname = True
+    #added the above in order to use ternary operators to include optional default filenames
+    cwdpath = (str(filedir) + "/" + "%(title)s.%(ext)s") if defaultname == True else (str(filedir) + "/" + str(filename) + ".%(ext)s")
+    defaultpath = ("/home/" + name + "/pythondir/yt2mp3/downloaded/" + "%(title)s.%(ext)s") if defaultname == True else ("/home/" + name + "/pythondir/yt2mp3/downloaded/" + str(filename) + "%.(ext)s")
+    params = {
+            'writethumbnail': True,
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3', 
+                'preferredquality': '192',},
+                {'key': 'EmbedThumbnail'},],# this makes thumbnaildownload obsolete, but I'll keep it in case.
+            'cookiefile' : '~/pythondir/yt2mp3/youtube.com_cookies.txt',
+            'source_address':'0.0.0.0',
+            'outtmpl':  (cwdpath) if heredeterminer == "yes" or "Yes" or "y" else (defaultpath)
+            #above is a really long line, with ternary operators. Useful for changing outtmpl's definition
+            #updated, put the paths in seperate variables with ternary operators of their own, for optional default names.
+}
+    youtube = youtube_dl.YoutubeDL(params) #the bulk of the work is made by youtube-dl.
+    ytname = str(ytname)
+    youtube.download([ytname])
+    titlefinder = youtube_dl.YoutubeDL({'outtmpl': '%(title)s'})
+    titlelist = titlefinder.extract_info(ytname, download=False)
+    titlename = titlelist.get("title", None)
+    try:
+        audioname = (filename+".mp3") if heredeterminer == "yes" or "Yes" or "y" else ("/home/" + name + "/pythondir/yt2mp3/downloaded/" + filename+".mp3")
+        audiofile = eyed3.load(audioname)
+    except:
+        audioname = (titlename+".mp3") if heredeterminer == "yes" or "Yes" or "y" else ("/home/" + name + "/pythondir/yt2mp3/downloaded/"+titlename+".mp3")
+        audiofile = eyed3.load(audioname)
+    #here, I'm adding eyed3 tagging, as the inbuilt youtube-dl tagging can be unreliable. It uses try except to find the right file using the yt title or the filename. 
+    artistfinder = input("Who is the artist?\n")#always remember your '\n's!
+    audiofile.tag.artist = str(artistfinder)
+    albumfinder = input("Album?\n")
+    audiofile.tag.album = str(albumfinder)
+    trackfinder = input("Track name?\n")
+    audiofile.tag.title = str(trackfinder)
+    audiofile.tag.save()
+    #Some simple querying in order to put the right tags. 
+    print("The thumbnail has been downloaded and embedded within the file. However, you can still download it seperately.")
+    determiner = input("Do you want to download the thumbnail?\n") #a little interactive determiner to download the thumbnail
+    if (determiner == "yes"):
+        seconddeter = input("Do you want to download it in the current directory?\n")
+        if seconddeter == "yes":
+            download_thumbnail(str(ytname), str(filename), "yes")
+        else:
+            download_thumbnail(str(ytname), str(filename), "no")
+    elif (determiner == 'no' or "No" or "n"):
+        exit(0)
+    else:
+        exit(1)
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) >= 2:
+        try:
+            download_music(sys.argv[1], sys.argv[2], sys.argv[3])
+        except:
+            try:
+                download_music(sys.argv[1], sys.argv[2])
+            except:
+                download_music(sys.argv[1])
+    else:
+        download_music()
+        #here, if I'm running the script directly, it tries running it with command line arguments
+        #Of course, if they are no arguments it throws an IndexError. So it moves on 
+        #here, now it just calls it without any sys.argv. everything is set to None, and the iterations go to work.
+
+#That's it! It should all be good now, so enjoy working on whatever projects you're on now, Zack!
