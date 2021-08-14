02a4d1f219003659f37fa3c3f154040f0d072958
ignition-ctrl <akazackt@gmail.com>
2021-08-14 14:44:21 -0400

    updated old repo to ssh

--- youtubeMP3.py
+++ youtubeMP3.py
@@ -15,11 +15,11 @@ def download_music(ytname=None, filename=None, heredeterminer=None):
             print("No link found; please try again")
             exit(1)
     else:
-        if not re.match("http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?", ytname):
+        if re.match("http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?", ytname):
+            print('Link detected')
+        else:
             newlinkfind = input("Not a valid link; please re-enter\n")
             ytname = str(newlinkfind)
-        else:
-            print("Link detected")
     name = getpass.getuser() 
     if filename == None:
         namefinder = input("Please enter your desired filename for the .mp3 file\n")
@@ -38,14 +38,23 @@ def download_music(ytname=None, filename=None, heredeterminer=None):
         print("Continuing with default filename")
     cwdpath = (str(filedir) + "/" + "%(title)s.%(ext)s") if defaultname == True else (str(filedir) + "/" + str(filename) + ".%(ext)s")
     defaultpath = ("/home/" + name + "/pythondir/yt2mp3/downloaded/" + "%(title)s.%(ext)s") if defaultname == True else ("/home/" + name + "/pythondir/yt2mp3/downloaded/" + str(filename) + "%.(ext)s")
+    lengthfinder = youtube_dl.YoutubeDL({'quiet': True, 'forceduration': True})
+    infolist = lengthfinder.extract_info(ytname, download=False)
+    vidlengthsec = infolist.get('duration', None)
+    vidlengthmin = (vidlengthsec / 60)
+    if vidlengthmin > 10:
+        print("Video length is " + str(vidlengthmin) +"\n Downloading as .m4a for best audio")
+        m4aformat = True
+    else:
+        m4aformat = False
     params = {
             'writethumbnail': True,
-            'format': 'bestaudio/best',
+            'format': 'bestaudio/best' if m4aformat == False else 'm4a',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3', 
                 'preferredquality': '192',},
-                {'key': 'EmbedThumbnail'},],
+                {'key': 'EmbedThumbnail'},] if m4aformat == False else [],
             'cookiefile' : '~/pythondir/yt2mp3/youtube.com_cookies.txt',
             'source_address':'0.0.0.0',
             'outtmpl':  (cwdpath) if heredeterminer == "yes" or "Yes" or "y" else (defaultpath)
@@ -54,9 +63,30 @@ def download_music(ytname=None, filename=None, heredeterminer=None):
     ytname = str(ytname)
     print("Downloading in current directory...")
     youtube.download([ytname])
-    titlefinder = youtube_dl.YoutubeDL({'outtmpl': '%(title)s'})
+    titlefinder = youtube_dl.YoutubeDL({'outtmpl': '%(title)s.%(ext)s'})
     titlelist = titlefinder.extract_info(ytname, download=False)
     titlename = titlelist.get("title", None)
+    if m4aformat == True:
+        print("Skipping eyeD3 tagging, assuming file is a mix")
+    else:
+        try:
+            audioname = (filename+".mp3") if heredeterminer == "yes" or "Yes" or "y" else ("/home/" + name + "/pythondir/yt2mp3/downloaded/" + filename+".mp3")
+            audiofile = eyed3.load(str(audioname))
+            print(audioname+" loaded\nEyeD3 tagging:")
+        except:
+            audioname = (titlename+".mp3") if heredeterminer == "yes" or "Yes" or "y" else ("/home/" + name + "/pythondir/yt2mp3/downloaded/"+titlename+".mp3")
+            audiofile = eyed3.load(str(audioname))
+            print(audioname+" loaded\nEyeD3 tagging:")
+        artistfinder = input("Who is the artist?\n")#always remember your '\n's!
+        audiofile.tag.artist = str(artistfinder)
+        albumfinder = input("Album?\n")
+        audiofile.tag.album = str(albumfinder)
+        trackfinder = input("Track name?\n")
+        audiofile.tag.title = str(trackfinder)
+        audiofile.tag.save()
+    #here, I'm adding eyed3 tagging, as the inbuilt youtube-dl tagging can be unreliable. It uses try except to find the right file using the yt title or the filename. 
+    
+=======
     try:
         audioname = (filename+".mp3") if heredeterminer == "yes" or "Yes" or "y" else ("/home/" + name + "/pythondir/yt2mp3/downloaded/" + filename+".mp3")
         audiofile = eyed3.load(audioname)
@@ -87,22 +117,24 @@ def download_music(ytname=None, filename=None, heredeterminer=None):
 
 
 if __name__ == '__main__':
-    # if len(sys.argv) >= 2:
-    #     try:
-    #         download_music(sys.argv[1], sys.argv[2], sys.argv[3])
-    #     except:
-    #         try:
-    #             download_music(sys.argv[1], sys.argv[2])
-    #         except:
-    #             download_music(sys.argv[1])
-    # else:
-    if (str(sys.argv[1:]) == "[]"):
-        print("No arguments")
-        download_music()
-    if (len(sys.argv) == 0):
-        print(__name__)
-        download_music()
-    elif (len(sys.argv) == 2):
-        download_music(sys.argv[1])
-    elif (len(sys.argv) == 3):
-        download_music(sys.argv[1], sys.argv[2])
+    try:
+        download_music(sys.argv[1], sys.argv[2], sys.argv[3])
+    except:
+        try:
+            download_music(sys.argv[1], sys.argv[2])
+        except:
+            if len(sys.argv) == 2:
+                download_music(sys.argv[1])
+            else:
+                print('No arguments')
+                download_music()
+    # if (str(sys.argv[1:]) == "[]"):
+    #     print("No arguments")
+    #     download_music()
+    # if (len(sys.argv) == 0):
+    #     print(__name__)
+    #     download_music()
+    # elif (len(sys.argv) == 2):
+    #     download_music(sys.argv[1])
+    # elif (len(sys.argv) == 3):
+    #     download_music(sys.argv[1], sys.argv[2])
